fb39058d62d23beaabd3de3fb4e6df3477d28fe4
Artin <58168246+artinalbert@users.noreply.github.com>
2023-01-29 12:11:47 +0100

    Initial implementation

---
+++ src/videoconverter/main.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import subprocess
+
+
+
+
+
+def main():
+    src_video_path = Path("/Users/artin/Downloads")
+    if src_video_path.exists():
+        for i in src_video_path.iterdir():
+            if i.suffix == ".mov":
+                output = Path("/Users/artin/Downloads/videoOutput")
+                file_name = i.name
+                if not output.exists():
+                    output.mkdir()
+                #print(f"ffmpeg -i '{i}' -c:v libx265 -preset veryfast -tag:v hvc1 '{Path(output,file_name)}'")
+                ffmpeg = subprocess.run(["ffmpeg",f"-i '{i}' -c:v libx265 -preset veryfast -tag:v hvc1 '{Path(output,file_name)}'"],check=False,capture_output=True)
+                print(ffmpeg.stdout)
+                print(ffmpeg.stderr)
+                exit()
+
+                
+        
+
+
+if __name__=="__main__":
+    main()
