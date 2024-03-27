a90be94be4eb673beb2c58d316ddfaeff9ee57a0
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2024-03-27 16:31:48 +0100

    wip

--- src/videoconverter/main.py
+++ src/videoconverter/main.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+import re
 import subprocess
+from types import FrameType
 from tqdm import tqdm
 from Foundation import NSFileManager, NSURL
+from argparse import ArgumentParser, Namespace
+import signal
 
 
 file_manager = NSFileManager.defaultManager()
+subprocesses: list[subprocess.Popen] = []
+
 
 def delete_file(file: Path):
     file_url = NSURL.fileURLWithPath_(str(file.resolve()))
@@ -14,58 +20,80 @@ def delete_file(file: Path):
 
 
 def get_video_files(dir: Path):
-    video_files = [f for f in tqdm(dir.iterdir(), desc="Collecting Files") if f.suffix == ".mov"]
+    video_files = [
+        f for f in tqdm(dir.iterdir(), desc="Collecting Files") if f.suffix == ".mov"
+    ]
     video_files.sort()
     return video_files
 
-def convert_video(input_file: Path, output_video_path: Path):
-    file_name = input_file.name
-    tqdm.write(f"Converting {file_name}")
-    ffmpeg = subprocess.Popen(
-        [
-            "ffmpeg",
-            f"-i",
-            f"{input_file}",
-            "-c:v",
-            "libx265",
-            "-preset",
-            "veryfast",
-            "-tag:v",
-            "hvc1",
-            f"{Path(output_video_path,file_name)}",
-        ],
-        bufsize=1,
-        stdout=subprocess.PIPE,
-        text=True
-    )
-    while ffmpeg.poll() is not None or ffmpeg:
-        tqdm.write(ffmpeg.stdout.readline())
 
-    # ffmpeg = subprocess.run(
-    #     [
-    #         "ffmpeg",
-    #         f"-i",
-    #         f"{input_file}",
-    #         "-c:v",
-    #         "libx265",
-    #         "-preset",
-    #         "veryfast",
-    #         "-tag:v",
-    #         "hvc1",
-    #         f"{Path(output_video_path,file_name)}",
-    #     ],
-    #     check=False,
-    #     capture_output=True,
-    # )
-    # tqdm.write(ffmpeg.stdout.decode())
-    # tqdm.write(ffmpeg.stderr.decode())
-    # if ffmpeg.returncode != 0:
-    #     raise Exception("Process failed")
-    # delete_file(input_file)
+def convert_video(input_file: Path, output_video_path: Path):
+    with tqdm(total=None, unit="frames", unit_scale=True) as pbar:
+        file_name = input_file.name
+        tqdm.write(f"Converting {file_name}")
+        ffmpeg = subprocess.Popen(
+            [
+                "ffmpeg",
+                f"-i",
+                f"{input_file}",
+                "-c:v",
+                "libx265",
+                "-preset",
+                "veryfast",
+                "-tag:v",
+                "hvc1",
+                f"{Path(output_video_path,file_name)}",
+            ],
+            bufsize=1,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        subprocesses.append(ffmpeg)
+        while ffmpeg.poll() is None:
+            line = ffmpeg.stderr.readline().replace("\n", "")
+            if line.startswith("frame"):
+                pattern = r"(\w+)=\s*([0-9]+(?:\.[0-9]*)?|[0-9]*\.[0-9]+)([a-zA-Z%]*)"
+                matches = re.findall(pattern, line)
+                line_info = {
+                    key: float(value) if value.isdigit() or "." in value else value
+                    for key, value, unit in matches
+                }
+                pbar.n = line_info["frame"]
+                pbar.update()
+            else:
+                tqdm.write(line)
+
+        returncode = ffmpeg.wait()
+        if returncode != 0:
+            raise subprocess.CalledProcessError(returncode, ffmpeg.args)
+
+
+def parse_arguments() -> Namespace:
+    # Create the parser
+    parser = ArgumentParser(description="Reencode videos to a more efficient format")
+
+    # Add the '-i' argument
+    parser.add_argument("-i", type=Path, help="input folder location")
+
+    # Parse the arguments
+    args = parser.parse_args()
+    return args
+
+
+def stop_subprocesses(sig: int, frame: FrameType | None):
+    for process in subprocesses:
+        process.terminate()
+        process.wait()
+    raise KeyboardInterrupt()
+
 
 def main():
-    src_video_path = Path("/Users/artin/Desktop")
-    output_video_path = Path("/Users/artin/Desktop/videoOutput")
+    args = parse_arguments()
+    src_video_path: Path = args.i
+    output_video_path = Path(src_video_path, "videoOutput")
+
+    signal.signal(signal.SIGINT, stop_subprocesses)
 
     if src_video_path.exists():
         if not output_video_path.exists():
